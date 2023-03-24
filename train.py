44afeb94eb4f5c18ca5548f7792e66b2d38e78c2
bakerhassan <61099003+bakerhassan@users.noreply.github.com>
2023-03-24 12:53:15 -0400

    Update train.py

--- train.py
+++ train.py
@@ -119,16 +119,6 @@ def train(args, dataset, generator, g_running, discriminator, mask_loss_fn, logg
     logger.log_images(real_samples, tag='real_samples', step=0, epoch=step)
     gen_i, gen_j = 8, 8
     fixed_noise = [torch.randn(gen_j, code_size).to(device) for _ in range(gen_i)]
-    if os.path.exists('./results/train.model'):
-        loaded_models = torch.load('./results/train.model')
-        generator.module.generator.load_state_dict(loaded_models['generator'])
-        discriminator.module.load_state_dict(loaded_models['discriminator'])
-        g_optimizer.load_state_dict(loaded_models['g_optimizer'])
-        d_optimizer.load_state_dict(loaded_models['d_optimizer'])
-        g_running.generator.load_state_dict(loaded_models['generator_running'])
-        alpha = loaded_models['alpha']
-        step = loaded_models['step']
-
     for i in pbar:
 
         d_optimizer.zero_grad()
